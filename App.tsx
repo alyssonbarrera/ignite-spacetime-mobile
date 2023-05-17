e97ef6c0b33fce21214330db62852215ff6a3c46
Alysson Barrera <alyssonbarrera@outlook.com>
2023-05-16 23:27:43 -0300

    ✨ add home screen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,78 @@
-import { StatusBar, Text, View } from 'react-native'
+import { styled } from 'nativewind'
+import {
+  ImageBackground,
+  StatusBar,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
 import { setBackgroundColorAsync } from 'expo-navigation-bar'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '@assets/blur-bg.png'
+import Stripes from '@assets/stripes.svg'
+import NLWLogo from '@assets/nlw-spacetime-logo.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
-  setBackgroundColorAsync('#030712')
+  setBackgroundColorAsync('#121215')
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-gray-50">Hello, World!</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-30%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.8}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-    </View>
+    </ImageBackground>
   )
 }
--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,22 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@routes': './src/routes',
+            '@utils': './src/utils',
+            '@libs': './src/libs',
+          },
+        },
+      ],
+    ],
   }
 }
